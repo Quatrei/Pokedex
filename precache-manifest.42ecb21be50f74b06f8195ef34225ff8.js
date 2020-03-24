76780feb4e5f48ea2b1247e4826e635af24e4e23
Quatrei <brianpi@magenic.com>
2020-03-24 11:47:50 +0000

    Updates

---
+++ precache-manifest.42ecb21be50f74b06f8195ef34225ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724bf1c8c36490e46d7174867366a00c",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "a77983ab0a9a08d45c0a",
+    "url": "/Pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df9cdd4bb6eda0954fa6",
+    "url": "/Pokedex/static/js/2.4072d654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.4072d654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77983ab0a9a08d45c0a",
+    "url": "/Pokedex/static/js/main.623ad027.chunk.js"
+  },
+  {
+    "revision": "398e5535d1092405d741",
+    "url": "/Pokedex/static/js/runtime-main.9a389de8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
